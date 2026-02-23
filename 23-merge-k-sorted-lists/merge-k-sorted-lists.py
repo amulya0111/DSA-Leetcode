e6d38e9a4b287b92e8f66c8d2263b3037ce79d44
Amulya Mishra <amulyamishra2006@gmail.com>
2026-02-23 21:51:53 +0530

    Time: 31 ms (17.82%) | Memory: 18.3 MB (51.56%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -4,38 +4,28 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def __init__(self,start=None):
-        self.start=start
-    def mergeKLists(self, lists):
-        # made a head node and assigned a temp
+    def mergetwolists(self, list1, list2):
         head=ListNode()
         temp=head
-        # making a while loop until the array has nothing
-        # ok so instead of writing: 
-        # while lists is not None:
-        # we will write
-        while any(lists):
-         # to help continue until all lists are exhausted 
-            # finding smallest
-            # first initialise smallest , its a list node so:
-            smallest =None
-            smallest_index=-1
-            for i in range(len(lists)):
-                if lists[i] is not None:
-                    if smallest is None or lists[i].val<smallest.val:
-                        smallest = lists[i]
-                        smallest_index=i
-                    # found the index to be incremented , now increment pointer 
-            # incrementing the list taken
-            lists[smallest_index]=lists[smallest_index].next
-            # assigning and incrementing temp
-            # not like this: 
-            # head.val=smallest
-            # BECAUSE SMALLEST IS A NODE NOT VALUE
-            temp.next=smallest
+        while list1 and list2:
+            if list1.val>=list2.val:
+                temp.next=list2
+                list2=list2.next
+            else:
+                temp.next=list1
+                list1=list1.next
             temp=temp.next
+        temp.next=list1 if list1 else list2
         return head.next
-
-
-
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        while len(lists) > 1: #eg: [l1,l2,l3,l4,l5]
+            merged = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if i+1 < len(lists) else None
+                merged.append(self.mergetwolists(l1, l2)) #appending (l1+l1,l3+l4,l5+None) i.e [m1,m2,m3]
+            lists = merged #i.e lists=[m1,m2,m3] and it will keep going ([m12,m3n]>[M]) until len lists is>1 , as [M] is found , it will return 
+        return lists[0]
         
