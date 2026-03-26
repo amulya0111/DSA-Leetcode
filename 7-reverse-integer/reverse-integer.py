9bccdc43eed0aeb82e394eec4393e7d0e3231370
Amulya Mishra <amulyamishra2006@gmail.com>
2026-03-26 23:59:52 +0530

    Time: 22 ms (25.99%) | Memory: 12.5 MB (38.31%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reverse(self, x):
+        n=abs(x)
+        rev=0
+        while n>0:
+            r=n%10
+            rev=rev*10+r
+            n=n/10
+        if x<0:
+            if (-2**31)<=-rev<=(2**31-1):
+                return -rev
+            else:
+                return 0
+        else:
+            if (-2**31)<=rev<=(2**31-1):
+                return rev
+            else:
+                return 0
+        
+        
