12b7de7593d7a53e1d767bde0b7128b483710cf9
amulya0111 <amulyamishra2006@gmail.com>
2026-08-21 01:53:48 +0000

    Code challenge submission for question: 81 Search in Rotated Sorted Array II

---
+++ 81 Search in Rotated Sorted Array II/81search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        r=len(nums)-1
+        if r+1==0:
+            return False
+        if r+1==1 and nums[r]==target:
+            return True
+        l=0
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return True 
+            if nums[l]==nums[mid]==nums[r]:
+                l+=1
+                r-=1
+            # Left half is sorted
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
+            # Right half is sorted
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+        return False
