401ec7d3d2cc9d91ec18811eec679f9f6e751de3
Amulya Mishra <amulyamishra2006@gmail.com>
2026-08-26 08:28:19 +0530

    Time: 7 ms (13.02%), Space: 12.5 MB (14.79%) - LeetHub

---
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def vowelConsonantScore(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        vowel=0
+        consonant=0
+        vowelset={'a','e','i','o','u'}
+        for char in s:
+            if char.isalpha():
+                if char in vowelset:
+                    vowel+=1
+                else:
+                    consonant+=1
+        if consonant>0:
+            return vowel//consonant
+        else:
+            return 0
