e57258d9814ee9654a229ba86b9e5cdd875a2d08
Amulya Mishra <amulyamishra2006@gmail.com>
2026-09-17 06:38:40 +0530

    Time: 94 ms (62.87%), Space: 13.8 MB (79.07%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,30 +3,30 @@ class Solution(object):
         l=0
         r=0
         count=k
-        ones=0
+        length=0
         ans=0
         while r<len(nums):
             if nums[r]==1:
-                ones+=1
+                length+=1
                 r+=1
             else:
                 if count>0:
-                    ones+=1
+                    length+=1
                     count-=1
                     r+=1
                 else:
-                    ans=max(ans,ones)
+                    ans=max(ans,length)
                     while count==0:
                         if nums[l]==0:
                             count+=1
-                        ones-=1
+                        length-=1
                         l+=1
-                    ones+=1
+                    length+=1
                     count-=1
                     r+=1
             
                             
-        return max(ans,ones)
+        return max(ans,length)
 
             
 
